f38845373d674fa090416864dc2e8bc851d178e2
Alex Racinski <aracinski2@gmail.com>
2020-12-21 20:11:02 -0600

    make single note selector file, delete numbered note selectors

--- src/components/NoteSelector2.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="note-selector">
-    <p class="note-selector-title">Second note...</p>
-    <p class="note-selector-timestamp">Timestamp here...</p>
-  </div>
-</template>
--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="note-selectors">
-    <NoteSelector1 />
-    <NoteSelector2 />
-    <NoteSelector3 />
+    <NoteSelector />
+    <NoteSelector />
+    <NoteSelector />
+
   </div>
 </template>
 
 <script>
-import NoteSelector1 from "./NoteSelector1.vue";
-import NoteSelector2 from "./NoteSelector2.vue";
-import NoteSelector3 from "./NoteSelector3.vue";
+import NoteSelector from "./NoteSelector.vue";
+
 export default {
   components: {
-    NoteSelector1,
-    NoteSelector2,
-    NoteSelector3,
+    NoteSelector,
   },
 };
 </script>
